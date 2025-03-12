86ef66bde80f13afb3621eaa57ec8f82c0a4cd61
Santigodev <shilton.santiago@gmail.com>
2025-03-11 21:55:04 -0300

    Instalando Icones - font-awesome

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,35 @@
 <template>  
-    <div>Home
-        <br>
-        <button @click="deslogar">Sair</button>
+  <div>
+
+    <div class="container mt-4 text-center">
+      <h2>Bem vindo, {{ nomeUsuarioLogado }}</h2>
     </div>
+
+      <br>
+      <button @click="deslogar">Sair</button>
+  </div>
 </template>
 
 <script>   
 export default {
-    methods: {
-        deslogar()
-        {
-            localStorage.removeItem('dados-usuario-logado');
-            this.$router.push('/login');
-        }
+  data(){
+    return {
+      nomeUsuarioLogado: ''
+    };
+  },
+  mounted(){
+    const usuarioLogado = localStorage.getItem('dados-usuario-logado');
+    if(usuarioLogado){
+      this.nomeUsuarioLogado = JSON.parse(usuarioLogado).nome;
     }
+  },
+  methods: {
+      deslogar()
+      {
+          localStorage.removeItem('dados-usuario-logado');
+          this.$router.push('/login');
+      }
+  }
 }
 </script>
 
