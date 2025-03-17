732cf226cca9d7dc5d89fa7b845a7318a7348059
Santigodev <shilton.santiago@gmail.com>
2025-03-17 20:19:08 -0300

    Listagem de usuários usando API + loading

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -9,8 +9,12 @@
             </router-link>
         </div>
 
+        <div v-if="carregando" class="text-center">
+            <i class="fa fa-spinner fa-spin"></i> Carregando...
+        </div>
+
         <!-- Tabela de listagem -->
-         <table class="table table-striped">
+         <table v-if="!carregando" class="table table-striped">
             <thead class="thead-dark">
                 <tr>
                     <th>Id</th>
@@ -22,20 +26,22 @@
                 </tr>
             </thead>
             <tbody>
-                <td>5</td>
-                <td>aluno</td>
-                <td>alu@gmail.com</td>
-                <td>1213131</td>
-                <td>Ativo</td>
-                <td>
-                    <button class="btn btn-primary btn-sm"> 
-                    <i class="fa fa-edit"></i> Editar
-                    </button>
+                <tr v-for=" usuario in usuarios" :key="usuario.id">
+                    <td>{{ usuario.id }}</td>
+                    <td>{{ usuario.nome }}</td>
+                    <td>{{ usuario.email }}</td>
+                    <td>{{ usuario.cpf }}</td>
+                    <td>{{ usuario.status }}</td>
+                    <td>
+                        <button class="btn btn-primary btn-sm"> 
+                        <i class="fa fa-edit"></i> Editar
+                        </button>
 
-                    <button class="btn btn-danger btn-sm ml-2"> 
-                    <i class="fa fa-trash"></i> Excluir
-                    </button>
-                </td>
+                        <button class="btn btn-danger btn-sm ml-2"> 
+                        <i class="fa fa-trash"></i> Excluir
+                        </button>
+                    </td>
+                </tr>
             </tbody>
          </table>
 
@@ -46,11 +52,30 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            usuarios: []
+            usuarios: [],
+            carregando: false,
         };
     },
+    mounted(){
+        this.carregarUsuarios();
+    },
     methods: {
+        async carregarUsuarios()
+        {
+            this.carregando = true;
         
+            try {
+                const response = await axios.get("https://localhost:7263/api/v1/usuarios/listar-todos");
+                
+                    setTimeout(() => {
+                        this.usuarios = response.data;
+                        this.carregando = false;
+                }, 3000);
+            } catch (error) {
+                console.log(error);
+                this.carregando = false;
+            } 
+        }
     }
 }
 </script>
